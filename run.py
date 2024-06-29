9fbb057f4946e7ee09aabe28d880a06767ac4642
bgropay <bgropay@gmail.com>
2024-06-29 19:44:51 +0700

    Update run.py

--- run.py
+++ run.py
@@ -106,7 +106,7 @@ with open(wordlist_path, "r", encoding="latin-1", errors="ignore") as wordlist_f
 for username in shadow_dict:
     hashed_password = shadow_dict[username][1]
     print(f"{g}[+] {w}Found username: {shadow_dict[username][0]}{r}")
-    print(f"{b}[*] {w}Cracking the password for username: {shadow_dict[username][0]}{r}")
+    print(f"{b}[*] {w}Cracking the password for username: {shadow_dict[username][0]}...{r}")
     password_found = False
     for password in passwords:
         password = password.strip()
